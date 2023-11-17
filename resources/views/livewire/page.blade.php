4e69d132f423879b60d04470d5ec4dc6a1a5944d
kreid <magakhamatkhanov@gmail.com>
2023-11-17 16:46:05 +0300

    changed page of post

--- resources/views/livewire/page.blade.php
+++ resources/views/livewire/page.blade.php
@@ -5,7 +5,7 @@
 <div class="mx-12">
     <div class="flex flex-col gap-4">
         @foreach($this->post->images as $img)
-            <img src="/storage/{{$img->img_path}}" alt="">
+            <img src="{{env('YANDEX_URL').$img->img_path}}" alt="">
         @endforeach
     </div>
 
