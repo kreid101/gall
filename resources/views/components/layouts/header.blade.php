baa0990a9b65994112f5360c86fd2814d7830e99
kreid <magakhamatkhanov@gmail.com>
2023-11-19 00:44:45 +0300

    added favicons

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -6,6 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="yandex-verification" content="27480f8f450a31ee" />
+    <link type="image/x-icon" rel="shortcut icon" href="favicons/favicon.ico">
+    <link type="image/png" sizes="16x16" rel="icon" href="favicons/favicon-16x16.png">
+    <link type="image/png" sizes="32x32" rel="icon" href="favicons/favicon-32x32.png">
+    <link rel="apple-touch-icon" href="favicons/apple-touch-icon.png">
 
     <title>{{ $title ?? 'NUIMG' }}</title>
     <meta name="description" content="" />
